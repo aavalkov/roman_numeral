cf83d162ac87f0c47bd34c8a27d3d62a6f880e69
Tanner Steward & Anna Valkov <student@epicodus.com>
2014-07-23 14:00:11 -0700

    works with two digit numbers

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,10 +1,20 @@
 def roman_numerals(number)
-  symbol_value = {"M"=>1000,"D"=>500,"C"=>100, "L"=>50, "X"=>10, "V"=>5, "I"=>1}
+  symbol_value = {"M"=>1000, "CM"=>900, "D"=>500,
+                 "CD"=> 400, "C"=>100, "XC"=> 90,
+                  "L"=>50, "XL"=>40, "X"=>10, "IX"=>9,
+                  "V"=>5, "VI"=>4, "I"=>1}
   number = number.to_i
-  #symbol_value.each do |roman, num|
+    result =""
     if number > 3999
       "It is not a Roman Numeral"
     else
-      number = "I"
-end
+      0.upto(number)
+      symbol_value.each do |roman, num|
+        if number >= num
+          result = roman
+          break
+        end
+      end
+    result
+    end
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -2,7 +2,11 @@ require('rspec')
 require('roman_numerals')
 
 describe('roman_numerals') do
-  it("takes a single didget number and retrns its roman numeral form") do
+  it("takes a single diget number and retrns its roman numeral form") do
     roman_numerals(1).should(eq("I"))
   end
+
+  it("takes a double diget number and returns its roman numeral form") do
+    roman_numerals(10).should(eq("X"))
+end
 end
