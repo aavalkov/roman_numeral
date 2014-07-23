9341a5be7f4626ca1baa34038f5ecef4e311dcc1
Tanner Steward & Anna Valkov <student@epicodus.com>
2014-07-23 14:20:27 -0700

    takes any number and returns its roman numeral form

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,20 +1,22 @@
 def roman_numerals(number)
   symbol_value = {"M"=>1000, "CM"=>900, "D"=>500,
-                 "CD"=> 400, "C"=>100, "XC"=> 90,
-                  "L"=>50, "XL"=>40, "X"=>10, "IX"=>9,
-                  "V"=>5, "VI"=>4, "I"=>1}
+               "CD"=> 400, "C"=>100, "XC"=> 90,
+                "L"=>50, "XL"=>40, "X"=>10, "IX"=>9,
+                "V"=>5, "VI"=>4, "I"=>1}
   number = number.to_i
     result =""
     if number > 3999
       "It is not a Roman Numeral"
     else
-      0.upto(number)
-      symbol_value.each do |roman, num|
+      0.upto(number) do
+     symbol_value.each do |roman, num|
         if number >= num
-          result = roman
+          result += roman
+            number -= num
           break
         end
       end
-    result
     end
+  end
+  result
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -7,6 +7,14 @@ describe('roman_numerals') do
   end
 
   it("takes a double diget number and returns its roman numeral form") do
-    roman_numerals(10).should(eq("X"))
+    roman_numerals(17).should(eq("XVII"))
 end
+
+  it("takes a tripple digit number and returns its roman numeral form") do
+    roman_numerals(133).should(eq("CXXXIII"))
+    end
+
+  it ("takes a four digit number and returns its roman numeral form") do
+    roman_numerals(2345).should(eq("MMCCCXLV"))
+  end
 end
