bb4ceaf07fab3c1d1aa7cc3fd1fa5284e1c5dd03
Tanner Steward & Anna Valkov <student@epicodus.com>
2014-07-23 14:59:18 -0700

    fixed typo in roman numeral

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -2,17 +2,17 @@ def roman_numerals(number)
   symbol_value = {"M"=>1000, "CM"=>900, "D"=>500,
                "CD"=> 400, "C"=>100, "XC"=> 90,
                 "L"=>50, "XL"=>40, "X"=>10, "IX"=>9,
-                "V"=>5, "VI"=>4, "I"=>1}
+                "V"=>5, "IV"=>4, "I"=>1}
   number = number.to_i
-    result =""
-    if number > 3999
-      "It is not a Roman Numeral"
-    else
-      0.upto(number) do
-     symbol_value.each do |roman, num|
+  result = ""
+  if number > 3999
+    "It is not a Roman Numeral"
+  else
+    0.upto(number) do
+      symbol_value.each do |roman, num|
         if number >= num
           result += roman
-            number -= num
+          number -= num
           break
         end
       end
@@ -20,3 +20,5 @@ def roman_numerals(number)
   end
   result
 end
+
+puts roman_numerals(44)
